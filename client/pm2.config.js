7f2be7743ef9fe7b1b4fb2bf0a181c1203550c45
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2025-11-09 21:50:14 +0600

    updated

---
+++ client/pm2.config.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+module.exports = {
+  apps: [
+    {
+      name: "Brainicmart server",
+      script: "pnpm",
+      args: "start",
+      instances: 1,
+      exec_mode: "cluster",
+      listen_timeout: 10000,
+      restart_delay: 10000,
+      env: {
+        NODE_ENV: "production",
+        PORT: 3005,
+      },
+    },
+  ],
+};
