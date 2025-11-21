b5919b7aac2b7daac89f031dd0f366a0d3dc0eaf
Abu bakar <abubakarseddek199@gmail.com>
2025-11-21 16:55:53 +0600

    optimize about page

--- client/src/app/(public)/about/advisors/page.tsx
+++ client/src/app/(public)/about/advisors/page.tsx
@@ -1,15 +1,8 @@
+import { TeamMember } from "@/@types/TeamMemberProps";
+import AdvisorHeader from "@/components/aboutPage/AdvisorHeader";
 import SSRLoadMoreData from "@/components/SSRLoadMoreData";
 import { TeamCard } from "@/components/TeamCard"
 import { apiUrl } from "@/config/constants";
-
-interface TeamMember {
-  id: string
-  title: string
-  name: string
-  occupation?: string
-  image: string
-  description?: string
-}
 interface AboutPageProps {
   searchParams?: Promise<{ limit?: string }>;
 }
@@ -17,18 +10,8 @@ export default function AdvisorsPage({ searchParams }: AboutPageProps) {
   return (
     <div>
       <main className="min-h-screen ">
-        {/* Header Section */}
-        <section className="border-b py-12 md:py-16">
-          <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="max-w-2xl">
-              <h1 className="text-4xl font-bold text-slate-900 md:text-5xl">উপদেষ্টা মন্ডলী</h1>
-              <p className="mt-4 text-lg text-slate-600">
-                আমাদের অভিজ্ঞ এবং নিবেদিত উপদেষ্টারা সংস্থার দিকনির্দেশনা এবং কৌশলগত পরামর্শ প্রদান করে থাকেন। তাদের বিভিন্ন ক্ষেত্রের
-                দক্ষতা আমাদের মিশন বাস্তবায়নে গুরুত্বপূর্ণ ভূমিকা রাখে।
-              </p>
-            </div>
-          </div>
-        </section>
+        {/*AdvisorsPage Header Section */}
+        <AdvisorHeader/> 
         <SSRLoadMoreData<TeamMember>
           apiUrl={`${apiUrl}/commitee`}
           searchParams={searchParams}
@@ -43,7 +26,6 @@ export default function AdvisorsPage({ searchParams }: AboutPageProps) {
                 </div>
               );
             }
-
             return (
               <div className="container mx-auto px-4 sm:px-6 lg:px-8">
               <div className="grid gap-8 sm:grid-cols-3 lg:grid-cols-3">
---
+++ client/src/components/aboutPage/AdvisorHeader.tsx
@@ -0,0 +1,16 @@
+const AdvisorHeader = () => {
+    return (
+        <section className="border-b py-12 md:py-16">
+            <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="max-w-2xl">
+                    <h1 className="text-4xl font-bold text-slate-900 md:text-5xl">উপদেষ্টা মন্ডলী</h1>
+                    <p className="mt-4 text-lg text-slate-600">
+                        আমাদের অভিজ্ঞ এবং নিবেদিত উপদেষ্টারা সংস্থার দিকনির্দেশনা এবং কৌশলগত পরামর্শ প্রদান করে থাকেন। তাদের বিভিন্ন ক্ষেত্রের
+                        দক্ষতা আমাদের মিশন বাস্তবায়নে গুরুত্বপূর্ণ ভূমিকা রাখে।
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
+export default AdvisorHeader
