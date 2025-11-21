61d539b56e514a9d275e9a184bee52744e8fe218
Abu bakar <abubakarseddek199@gmail.com>
2025-11-21 18:18:28 +0600

    use relatedblog component in blog page

--- client/src/app/(public)/blog/[slug]/page.tsx
+++ client/src/app/(public)/blog/[slug]/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { apiUrl } from "@/config/constants";
 import type { IblogPosts } from "@/@types/BlogPostProps";
 import BlogDetails from "@/components/blogpage/BlogDetails";
+import RelatedBlog from "@/components/blogpage/RelatedBlog";
 interface BlogDetailPageProps {
   params: Promise<{
     slug: string;
@@ -106,44 +107,7 @@ export default function BlogDetailPage({ params }: BlogDetailPageProps) {
           {/* Left Side: Blog Detail */}
           <BlogDetails selectedPost={selectedPost} />
           {/* Right Side: Related Blogs */}
-          <div className="space-y-4">
-            <h2 className="text-xl font-bold mb-4 text-primary">অন্যান্য ব্লগ</h2>
-            <p className="text-muted-foreground mb-4 lg:mb-8">
-              আমাদের সাথে যুক্ত হতে পারেন এবং সমাজের উন্নয়নে অংশ নিন।
-            </p>
-            <div className="space-y-3">
-              {relatedPosts.map((post) => (
-                <div
-                  key={post._id}
-                  className="flex items-center gap-3 cursor-pointer hover:bg-secondary p-2 rounded transition"
-                  onClick={() => {
-                    setSelectedPost(post);
-                    window.history.pushState({}, "", `/blog/${post.slug}`);
-                  }}
-                >
-                  <div className="relative aspect-square w-[100px] h-[80px] overflow-hidden rounded">
-                    <Image
-                      src={post.image || "/placeholder.svg"}
-                      alt={post.title}
-                      fill
-                      className="object-cover"
-                    />
-                  </div>
-                  <div className="flex-1 min-w-0">
-                    <h3 className="text-sm font-semibold line-clamp-2">
-                      {post.title}
-                    </h3>
-                    <p className="text-xs text-muted-foreground">{post.date}</p>
-                  </div>
-                </div>
-              ))}
-              {/* {loadingMore && (
-                <p className="text-center text-muted-foreground py-3">
-                  আরও লোড হচ্ছে...
-                </p>
-              )} */}
-            </div>
-          </div>
+          <RelatedBlog relatedPosts={relatedPosts} setSelectedPost={setSelectedPost} />
         </div>
       </div>
     </main>
