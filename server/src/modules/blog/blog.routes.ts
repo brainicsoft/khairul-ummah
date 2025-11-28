d130ca7b95716820768466dc794f28d8397c59eb
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2025-11-29 02:40:09 +0600

    secure part done

--- server/src/modules/blog/blog.routes.ts
+++ server/src/modules/blog/blog.routes.ts
@@ -4,13 +4,14 @@ import { Router } from 'express';
 import { createBlogController, deleteBlogByIdController, getAllBlogController, getBlogBySlugController, updateBlogByIdController } from './blog.controller';
 import uploader from '../../utils/uploader/uploader';
 import { formValidator } from '../../middlewares/formVaidator';
+import auth from '../../middlewares/auth';
 
 export const blogRoutes: Router = Router();
 
 blogRoutes.get('/',getAllBlogController);
-blogRoutes.post('/request',uploader({ image: 'single' }),formValidator,createBlogController);
-blogRoutes.delete('/:id',deleteBlogByIdController);
-blogRoutes.patch('/:id',uploader({ image: 'single' }),formValidator, updateBlogByIdController);
+blogRoutes.post('/request',auth('admin'), uploader({ image: 'single' }),formValidator,createBlogController);
+blogRoutes.delete('/:id',auth('admin'), deleteBlogByIdController);
+blogRoutes.patch('/:id',auth('admin'),uploader({ image: 'single' }),formValidator, updateBlogByIdController);
 blogRoutes.get('/:slug', getBlogBySlugController);
 
   
--- server/src/modules/payment/payment.routes.ts
+++ server/src/modules/payment/payment.routes.ts
@@ -9,15 +9,16 @@ import {
   sslcommerzIPNController,
   getPaymentStatusController,
 } from "./payment.controller"
+import auth from "../../middlewares/auth"
 
 export const paymentRoutes: Router = Router()
 
 // Create payment (bKash or SSLCommerz)
 paymentRoutes.post("/create", createPaymentController)
 // Get all payments
-paymentRoutes.get("/", getAllPaymentController)
+paymentRoutes.get("/",auth('admin'), getAllPaymentController)
 // Get payment summary
-paymentRoutes.get("/summary", getPaymentSummaryController)
+paymentRoutes.get("/summary",auth('admin'), getPaymentSummaryController)
 // Get specific payment status
 paymentRoutes.get("/:id/status", getPaymentStatusController)
 // bKash verification (callback)
